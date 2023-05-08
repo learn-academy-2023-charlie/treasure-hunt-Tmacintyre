75b53d506d0611c0b8262939a37c3bc5ec4b1e50
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-05-08 13:51:49 -0400

    alert message showing

--- src/components/Square.js
+++ src/components/Square.js
@@ -6,7 +6,11 @@ const Square = (props) => {
     <div id="grid">
       {props.board.map((value, index) => {
             return(
-            <div className="square" key={index}>
+            <div 
+            className="square" 
+            key={index}
+            onClick={() => {props.handleClick(index)}}
+            >
                 {value}
             </div>
     
